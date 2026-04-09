9e85f5c3362df6f7feb6de8164e699fee418d24f
enzovene <enzovenerod@gmail.com>
2026-04-09 15:17:25 -0300

    exercicio 2 - Enzo

--- funcoes.py
+++ funcoes.py
@@ -10,12 +10,9 @@ def rolar_dados(n):
 # Exercício 2
 def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
     novos_dados = []
-    validação = False
     dados_no_estoque.append(dados_rolados[dado_para_guardar])
     for i in range(len(dados_rolados)):
-        if dados_rolados[i] != dados_rolados[dado_para_guardar] or validação == True:
+        if i != dado_para_guardar
             novos_dados.append(dados_rolados[i])
-        else:
-            validação = True
     resp = [novos_dados, dados_no_estoque]
     return resp
