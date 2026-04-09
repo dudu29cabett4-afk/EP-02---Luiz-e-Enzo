79196a138ce9a6b7411214cef0827b40277c702e
dudu29cabett4-afk <dudu29cabett4@gmail.com>
2026-04-09 17:02:34 -0300

    exercicio3 - luiz

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,5 @@
 # Exercício 1
+
 import random
 
 def rolar_dados(n):
@@ -8,6 +9,7 @@ def rolar_dados(n):
     return lista
 # -------------------------------------------------------------
 # Exercício 2
+
 def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
     novos_dados = []
     dados_no_estoque.append(dados_rolados[dado_para_guardar])
@@ -16,3 +18,14 @@ def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
             novos_dados.append(dados_rolados[i])
     resp = [novos_dados, dados_no_estoque]
     return resp
+
+# -------------------------------------------------------------
+# Exercício 3
+
+def remover_dado(lista_dados_rolados, lista_dados_estoque, n_indice):
+    lista = []
+    dado_removido = lista_dados_estoque.pop(n_indice)
+    lista_dados_rolados.append(dado_removido)
+    lista.append(lista_dados_rodados)
+    lista.append(lista_dados_estoque)
+    return lista
